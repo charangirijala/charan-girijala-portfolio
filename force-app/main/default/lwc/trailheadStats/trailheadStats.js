dfb98a52abee3c4428f7a5d75586fbdf0cc8e649
Akashreddy19 <girijalac@gmail.com>
2024-07-23 21:25:56 +0530

    included trailhead-stats

---
+++ force-app/main/default/lwc/trailheadStats/trailheadStats.js
@@ -0,0 +1,20 @@
+import { LightningElement } from "lwc";
+import { loadStyle } from "lightning/platformResourceLoader";
+import Ninjabootstrap from "@salesforce/resourceUrl/Ninjabootstrap";
+
+export default class TrailheadStats extends LightningElement {
+  bootStrapLoaded = false;
+  //loading bootstrap
+  renderedCallback() {
+    if (!this.bootStrapLoaded) {
+      Promise.all([loadStyle(this, Ninjabootstrap)])
+        .then(() => {
+          this.bootStrapLoaded = true;
+          console.log("Ninjabootstrap Loaded");
+        })
+        .catch((err) => {
+          console.log("Err loading bootstrap in portfolioBanner", err);
+        });
+    }
+  }
+}
